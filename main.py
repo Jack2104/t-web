ca2e535ec967675b9d6a0187f076028ceb2fb6b2
Jack Hill <jackhill2104@gmail.com>
2021-09-10 23:44:01 +0930

    lol

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ class WebPage(Page):
         self.url = url
         self.page_links = []
 
+        history.append(url)
+
     def parse_tag(self, tag):
         # This will occur if the link is standalone (i.e. not in-line)
         if tag.name == "a":
@@ -104,73 +106,6 @@ class WebPage(Page):
         return page_content
 
 
-def display_page(url):
-    global page_links
-
-    history.append(url)
-
-    def parse_tag(tag):
-        # This will occur if the link is standalone (i.e. not in-line)
-        if tag.name == "a":
-            page_links.append(tag.get("href"))
-            return TAG_STYLES["a"] % (len(page_links), tag.text) + "\n\n"
-
-        tag_content = ""
-        hypertext = []
-        # We only want the top-level children
-        children = tag.find_all(recusive=False)
-
-        # Get all of the in-line links (<a> tags)
-        if tag.name in TAG_STYLES and children:
-            # tag.find_all is used because we want this to get every child using recursive=True
-            for child_tag in tag.find_all():
-                if child_tag.name == "a":
-                    hypertext.append(child_tag.text)
-                    page_links.append(child_tag.get("href"))
-
-        if tag.name in TAG_STYLES:
-            tag_content = TAG_STYLES[tag.name] % tag.text
-
-            # Loop through every in-line link, re-format it, then add it back in to the content
-            for index in range(len(hypertext)):
-                # Get the number of the current link (note: not the index in page_links)
-                link_number = len(page_links) - len(hypertext) + index + 1
-                new_link = TAG_STYLES["a"] % (link_number, hypertext[index])
-
-                tag_content = tag_content.replace(hypertext[index], new_link)
-        elif children:
-            # Recursively call this function to parse all of the children
-            for index in range(len(children)):
-                child_tag = children[index]
-                tag_content += parse_tag(child_tag)
-
-                # Add two newlines if the child is the final list item
-                if child_tag.name in list_tags and index == len(children) - 1:
-                    tag_content += "\n"
-
-        return tag_content
-
-    os.system("clear")
-
-    try:
-        request = requests.get(url)
-        soup = BeautifulSoup(request.text, "lxml")
-    except:
-        console.print("[red]This page cannot be displayed.[/]")
-        return
-
-    body = soup.body  # Every website should have this tag
-    page_content = ""
-
-    for tag in body.find_all(recursive=False):
-        try:
-            page_content += parse_tag(tag)
-        except:
-            pass
-
-    console.print(page_content)
-
-
 def get_bookmarks():
     if not os.path.exists("./bookmarks.json"):
         with open("bookmarks.json", "w+") as json_file:
@@ -186,6 +121,7 @@ if __name__ == "__main__":
 
     os.system("clear")
 
+    # Create an event loop, of sorts
     while True:
         query = input("Search: ")
         search_terms = query.split()
@@ -203,15 +139,16 @@ if __name__ == "__main__":
 
             with open("bookmarks.json", "w+") as json_file:
                 json.dump(bookmarks, json_file)
+
         elif search_terms[0] == "-q" and len(search_terms) == 1:
             sys.exit()
-        else:
-            os.system("clear")
 
+        else:
             # For now, only supports urls (not organic searches)
             url = query.join(search_terms)
 
-            display_page(url)
+            web_page = WebPage(url)
+            web_page.display_page()
             print()
 
 # Test URL 1: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#going-down
